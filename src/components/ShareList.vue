0f08c2be197ad52e3fbd14229b6a550149e7ff0b
korostil <av.korostil@gmail.com>
2022-06-07 00:34:07 +0300

    Add simple search to share list page

--- src/components/ShareList.vue
+++ src/components/ShareList.vue
@@ -2,14 +2,32 @@
 import { ref } from 'vue';
 import ShareItem from '@/components/ShareItem';
 
-const shares = ref([
+const test_data = [
   { id: 1, title: 'Tesla', ticker: 'TSLA' },
   { id: 2, title: 'Yandex', ticker: 'YNDX' },
   { id: 3, title: 'Yandex', ticker: 'YNDXD' },
-]);
+];
+const shares = ref(Object.assign([], test_data));
+const search_text = ref();
+function search() {
+  let search_str = search_text.value.toLowerCase(),
+    shares_list = Object.assign([], test_data);
+  shares.value = shares_list.filter(
+    share =>
+      share.title.toLowerCase().includes(search_str) ||
+      share.ticker.toLowerCase().includes(search_str),
+  );
+}
 </script>
 
 <template>
+  <div>
+    <input
+      v-model="search_text"
+      placeholder="Введите название или тикер инструмента"
+    />
+    <button @click="search">Искать</button>
+  </div>
   <div>
     <ul>
       <ShareItem :share="share" v-for="share of shares" :key="share.id" />
